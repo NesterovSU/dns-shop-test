ee3e48680d945abe9b868d2a02d4947e0b47b4c7
Sergey Nesterov <nesterov_s_u@mail.ru>
2022-03-18 10:57:06 +0300

    add .properties parametrizing

--- src/main/java/managers/DriverManager.java
+++ src/main/java/managers/DriverManager.java
@@ -2,6 +2,8 @@ package managers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,17 +12,34 @@ import java.util.concurrent.TimeUnit;
  */
 public class DriverManager {
     private static WebDriver webDriver;
+    private static PropertiesManager properties = PropertiesManager.getInstance();
 
     public static WebDriver getWebDriver() {
         return webDriver != null ? webDriver : getInstance();
     }
 
     private static WebDriver getInstance() {
-        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\browser-drivers\\chromedriver98.exe");
-        webDriver = new ChromeDriver();
+
+        String browser = properties.get("browser.type");
+        switch (browser == null ? "chrome" : browser) {
+            case "chrome":
+                webDriver = new ChromeDriver();
+                break;
+            case "firefox":
+                webDriver = new FirefoxDriver();
+                break;
+            case "edge":
+                webDriver = new EdgeDriver();
+                break;
+            default:
+                webDriver = new ChromeDriver();
+        }
+
+        long implWait = Long.parseLong(properties.get("implicitly.wait"));
+        long pgLdTt = Long.parseLong(properties.get("page.load.timeout"));
+        webDriver.manage().timeouts().implicitlyWait(implWait, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().pageLoadTimeout(pgLdTt, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        webDriver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         return webDriver;
     }
 
